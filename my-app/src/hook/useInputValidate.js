9d224922637cabe238b65772e706b186ea20a7ed
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-23 20:42:45 +0500

    custom hook

---
+++ my-app/src/hook/useInputValidate.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+const useInputValidation = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (e) => setValue(e.target.value)
+
+    const validateInput = () => value.search(/\d/) >= 0;
+
+    const validateColor = validateInput() ? 'text-danger' : null
+    return {value, onChange, validateInput, validateColor}
+}
+
+export default useInputValidation;
